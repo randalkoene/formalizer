ec561c0704857a68c9446243a7538ad083381a24
randalk <randal.a.koene@gmail.com>
2021-01-10 23:09:17 -0800

    Added Log conversion and regeneration integrity test.

--- tools/conversion/graph2dil/graph2dil-integrity.py
+++ tools/conversion/graph2dil/graph2dil-integrity.py
@@ -70,22 +70,15 @@ def parse_options():
 
     parser = argparse.ArgumentParser(description='Test the integrity of graph2dil conversion by comparing generated HTML files\nwith original Formalizer 1.x HTML files.',epilog=theepilog)
     parser.add_argument('-i', '--inspector', dest='inspector', help='specify inspector program (default=less)')
+    parser.add_argument('-G', '--graph', dest='graph', action="store_true", help='Compare original and regenerated Graph')
+    parser.add_argument('-L', '--log', dest='log', action="store_true", help='Compare original and regenerated Log')
     parser.add_argument('-V', '--verbose', dest='verbose', action="store_true", help='turn on verbose mode')
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    get_graph2dil_config()
-    global args
-    args = parse_options()
-    
-    retcode = try_subprocess_check_output('graph-resident')
-    if (retcode != 0):
-        print('Missing memory-resident Graph. Please start \'fzserverpq\' to retry.')
-        sys.exit(retcode)
-
-    run_graph2dil = input('Run graph2dil (no skips this step)? (Y/n) ')
+def compare_Graph_original_and_regenerated():
+    run_graph2dil = input('Run graph2dil on Graph (no skips this step)? (Y/n) ')
     if (run_graph2dil != 'n'):
         thecmd = 'graph2dil -D'
         if args.verbose:
@@ -101,7 +94,7 @@ if __name__ == '__main__':
         print('Stopping.')
         sys.exit(0)
     
-    thecmd = f'graph2dil-diff.sh {DILfilesdir} {config["DILTLdirectory"]}/lists'
+    thecmd = f'graph2dil-diff.sh -G {DILfilesdir} {config["DILTLdirectory"]}/lists'
     #retcode = try_subprocess_check_output(thecmd)
     retcode = pty_process(thecmd)
     if (retcode != 0):
@@ -121,6 +114,62 @@ if __name__ == '__main__':
     if (retcode != 0):
         print('Inspector failed.')
         sys.exit(retcode)
+
+
+def compare_Log_original_and_regenerated():
+    run_graph2dil = input('Run graph2dil on Log (no skips this step)? (Y/n) ')
+    if (run_graph2dil != 'n'):
+        thecmd = 'graph2dil -L'
+        if args.verbose:
+            thecmd += ' -V'
+        retcode = pty_process(thecmd)
+        #retcode = try_subprocess_check_output(thecmd)
+        if (retcode != 0):
+            print('graph2dil failed.')
+            sys.exit(retcode)
+    
+    run_diff = input('Ready to run graph2dil-diff.sh (no exits)? (Y/n) ')
+    if (run_diff == 'n'):
+        print('Stopping.')
+        sys.exit(0)
+    
+    thecmd = f'graph2dil-diff.sh -L {DILfilesdir} {config["DILTLdirectory"]}/lists'
+    #retcode = try_subprocess_check_output(thecmd)
+    retcode = pty_process(thecmd)
+    if (retcode != 0):
+        print('graph2dil-diff.sh failed.')
+        sys.exit(retcode)
+
+    run_inspector = input('Ready to inspect diff file (no exits)? (Y/n) ')
+    if (run_inspector == 'n'):
+        print('Stopping.')
+        sys.exit(0)
+    
+    thecmd = 'less /tmp/graph2dil.diff'
+    if args.inspector:
+        thecmd = f'{args.inspector} /tmp/graph2dil.diff'
+    #retcode = try_subprocess_check_output(thecmd)
+    retcode = pty_process(thecmd)
+    if (retcode != 0):
+        print('Inspector failed.')
+        sys.exit(retcode)
+
+
+if __name__ == '__main__':
+    get_graph2dil_config()
+    global args
+    args = parse_options()
+    
+    retcode = try_subprocess_check_output('graph-resident')
+    if (retcode != 0):
+        print('Missing memory-resident Graph. Please start \'fzserverpq\' to retry.')
+        sys.exit(retcode)
+
+    if args.graph:
+        compare_Graph_original_and_regenerated()
+
+    if args.log:
+        compare_Log_original_and_regenerated()
 
     print('Done')
     sys.exit(0)
