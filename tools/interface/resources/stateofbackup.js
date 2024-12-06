d2b2985e13af8c13422690c5474863ce40868936
randalk <randal.a.koene@gmail.com>
2024-12-06 04:01:24 -0800

    Minor improvements.

--- tools/interface/resources/stateofbackup.js
+++ tools/interface/resources/stateofbackup.js
@@ -13,12 +13,31 @@ class backupState {
         }
     }
 
+    getFromDateStamp(datestampstr) {
+        const yearstr = datestampstr.substring(0,4);
+        const monthstr = datestampstr.substring(4,6);
+        const daystr = datestampstr.substring(6,8);
+        const hourstr = datestampstr.substring(8,10);
+        const minstr = datestampstr.substring(10,12);
+        const secstr = datestampstr.substring(12,14);
+        const dateString = `${yearstr}-${monthstr}-${daystr}T${hourstr}:${minstr}:${secstr}`; 
+        return new Date(dateString);
+    }
+
     updateState() {
         fetch(this.signalfile)
         .then(response => response.text())
         .then(text => {
             this.state = text;
+            const currentdatetime = new Date();
+            const signaldatetime = this.getFromDateStamp(text);
+            const ms = currentdatetime.getTime() - signaldatetime.getTime();
             this.stateelement.textContent = text;
+            if (ms > 86400000) {
+                this.stateelement.style.color = '#ff0000';
+            } else {
+                this.stateelement.style.color = '#00af00';
+            }
         })
         .catch(error => {
             console.error('backupState error:', error);
