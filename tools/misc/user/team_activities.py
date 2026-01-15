3812c821e801c84f995d173b3e95872145eff108
randalk <randal.a.koene@gmail.com>
2026-01-15 02:50:45 -0800

    Addded node=all option for team_activities.py.

--- tools/misc/user/team_activities.py
+++ tools/misc/user/team_activities.py
@@ -136,6 +136,15 @@ def get_superiors(node_data:dict)->list:
         return []
     return superiors
 
+def get_dependencies(node_data:dict)->list:
+    if 'dependencies' not in node_data:
+        return []
+    try:
+        dependencies = node_data['dependencies'].strip().split(' ')
+    except:
+        return []
+    return dependencies
+
 ACTIVITIES_PAGE = '''<html>
 <head>
 <meta charset="utf-8" />
@@ -164,6 +173,30 @@ Involved in superiors due: %s
 </html>
 '''
 
+TEAM_ACTIVITIES_PAGE = '''<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<link rel="icon" href="/favicon-32x32.png">
+<link rel="stylesheet" href="/fz.css">
+<link rel="stylesheet" href="/fzuistate.css">
+<title>Team Activities</title>
+</head>
+<body>
+<script type="text/javascript" src="/fzuistate.js"></script>
+<h1>Team Activities</h1>
+<p>
+<pre>
+%s
+</pre>
+</p>
+</body>
+</html>
+'''
+
 def td_beyond(td:str)->bool:
     target_date = datetime.strptime(td, "%Y%m%d%H%M")
     now = datetime.now()
@@ -171,8 +204,7 @@ def td_beyond(td:str)->bool:
     days_remaining = delta.days
     return days_remaining > targetdate_relevance_days
 
-def process_common(node:str):
-    global json_output
+def collect_member_activities(node:str)->dict:
     node_data = get_node_data(node)
     # Collect CCF skills application
     ccfskills_str = extract_ccf_skill_application(node_data['node-text']).strip()
@@ -198,14 +230,32 @@ def process_common(node:str):
         'activities': current_activities_list,
         'superiors': relevant_superiors,
     }
-    if outformat=='html':
-        print(ACTIVITIES_PAGE % (
-            collected_activities['ccfskills'],
-            ', '.join(collected_activities['activities']),
-            ', '.join(collected_activities['superiors'])))
+    return collected_activities
+
+def process_common(node:str, team:str):
+    global json_output
+    if node=='all':
+        team_node_data = get_node_data(team)
+        team_members = get_dependencies(team_node_data)
+        team_activities = {}
+        for member in team_members:
+            member_activities = collect_member_activities(member)
+            team_activities[member] = member_activities
+        json_output = team_activities
+        if outformat=='html':
+            print(TEAM_ACTIVITIES_PAGE % json.dumps(json_output, indent=4))
+        else:
+            print(json.dumps(json_output))
     else:
-        json_output = collected_activities
-        print(json.dumps(json_output))
+        collected_activities = collect_member_activities(node)
+        if outformat=='html':
+            print(ACTIVITIES_PAGE % (
+                collected_activities['ccfskills'],
+                ', '.join(collected_activities['activities']),
+                ', '.join(collected_activities['superiors'])))
+        else:
+            json_output = collected_activities
+            print(json.dumps(json_output))
 
 def process_as_cgi():
     global outformat
@@ -228,8 +278,11 @@ def process_as_cgi():
             targetdate_relevance_days = int(tdays_relevant)
         except:
             targetdate_relevance_days = 30
+    team = form.getvalue('team')
+    if not taem:
+        team = "20250822084723.1"
 
-    process_common(node)
+    process_common(node, team)
 
 def process_as_cmdline():
     global outformat
@@ -237,7 +290,8 @@ def process_as_cmdline():
     import argparse
 
     parser = argparse.ArgumentParser(description='Collect CCF Team activities information.')
-    parser.add_argument('-n', dest='node', type=str, help='Team Node to collect activities information about')
+    parser.add_argument('-n', dest='node', type=str, help='Team Node to collect activities information about ("all" means for the whole team)')
+    parser.add_argument('-s', dest='team', type=str, default="20250822084723.1", help='Superior Node in which to find Team members available (default: 20250822084723.1)')
     parser.add_argument('-T', dest='tdays_relevant', type=int, default=30, help='Days to target date considered relevant (default: 30)')
     parser.add_argument('-F', dest='outformat', type=str, default='json', help='Output format: json (default)')
     parser.add_argument('-v', '--verbose', dest='verbose', action="store_true", help='turn on verbose mode')
@@ -246,7 +300,7 @@ def process_as_cmdline():
     outformat = args.outformat
     targetdate_relevance_days = args.tdays_relevant
 
-    process_common(args.node)
+    process_common(args.node, args.team)
 
 
 if __name__ == '__main__':
